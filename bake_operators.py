36eb6329471dd7e0a2569bbe47558a3403c728f4
David Gayerie <dagaydevel@free.fr>
2018-01-21 18:22:58 +0100

    another fix for bake of steering

--- bake_operators.py
+++ bake_operators.py
@@ -245,24 +245,6 @@ class BakeSteeringOperator(bpy.types.Operator, BakingOperator):
                 self._bake_steering_rotation(context, distance, mch_steering_rotation)
         return {'FINISHED'}
 
-    def _compute_next_pos(self, frame, loc_evaluator):
-        pos = loc_evaluator.evaluate(frame)
-        next_frame = frame + 1
-        next_pos = loc_evaluator.evaluate(next_frame)
-        tangent_vector = next_pos - pos
-        while tangent_vector.length < 1:
-            if next_frame >= self.frame_end:
-                break
-            next_frame = next_frame + 1
-            next_pos = loc_evaluator.evaluate(next_frame)
-            tangent_vector = next_pos - pos
-
-        # simple linear interpolation
-        if tangent_vector.length > 0:
-            next_frame = frame + (next_frame - frame) / tangent_vector.length
-
-        return mathutils.Vector(loc_evaluator.evaluate(next_frame))
-
     def _evaluate_rotation_per_frame(self, action, bone):
         loc_evaluator = self._create_location_evaluator(action, bone)
         rot_evaluator = self._create_rotation_evaluator(action, bone)
@@ -270,19 +252,26 @@ class BakeSteeringOperator(bpy.types.Operator, BakingOperator):
         init_vector = bone.head - bone.tail
         current_pos = loc_evaluator.evaluate(self.frame_start)
         prev_rotation = .0
+        last_frame = self.frame_start
         for f in range(self.frame_start, self.frame_end - 1):
-            next_pos = self._compute_next_pos(f, loc_evaluator)
-
+            next_pos = loc_evaluator.evaluate(f + 1)
             world_space_tangent_vector = next_pos - current_pos
             local_space_tangent_vector = rot_evaluator.evaluate(f).inverted() * world_space_tangent_vector
-
             current_rotation = local_space_tangent_vector.xy.angle_signed(init_vector.xy, prev_rotation)
-            if abs(prev_rotation - current_rotation) > self.keyframe_tolerance / 100 or f == self.frame_start:
-                if f > 1:
-                    yield f - 1, prev_rotation
+            # TODO better guess the minimun length and remove some wrong rotation
+            if (next_pos - current_pos).length < .02:
+                drop_keyframe = True
+            else:
+                drop_keyframe = abs(prev_rotation - current_rotation) < self.keyframe_tolerance / 50
+            if not drop_keyframe or f == self.frame_start:
+                # TODO should also take speed into account
+                anticipation = abs(prev_rotation - current_rotation) * 7
+                if f - last_frame > anticipation:
+                    yield f - anticipation, prev_rotation
                 yield f, current_rotation
-            prev_rotation = current_rotation
-            current_pos = next_pos
+                last_frame = f
+                prev_rotation = current_rotation
+                current_pos = next_pos
 
         yield self.frame_end, prev_rotation
 
