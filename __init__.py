f23b4550fd0eebd0a7cee1c347a5c68de42c6ac4
David <dagaydevel@free.fr>
2018-11-07 23:43:36 +0100

    new panels and layout for blender 2.80

--- __init__.py
+++ __init__.py
@@ -18,11 +18,6 @@
 
 # <pep8 compliant>
 
-import bpy
-from . import bake_operators
-from . import car_rig
-
-
 bl_info = {
     "name": "Rigacar (Generates Car Rig)",
     "author": "David Gayerie",
@@ -52,73 +47,155 @@ else:
 class BaseCarRigPanel:
 
     @classmethod
-    def poll(cls, context):
-        return 'Car Rig' in context.object.data if context.object is not None and context.object.data is not None else False
+    def is_car_rig(cls, context):
+        return context.object is not None and context.object.data is not None and 'Car Rig' in context.object.data
 
-    def draw(self, context):
-        if context.object.data['Car Rig']:
-            self.layout.operator(bake_operators.BakeSteeringOperator.bl_idname)
-            self.layout.operator(bake_operators.BakeWheelRotationOperator.bl_idname)
-            self.layout.operator(bake_operators.ClearSteeringWheelsRotationOperator.bl_idname)
+    @classmethod
+    def is_car_rig_generated(cls, context):
+        return cls.is_car_rig(context) and context.object.data['Car Rig']
+
+    def display_generate_section(self, context):
+        self.layout.operator(car_rig.GenerateCarAnimationRigOperator.bl_idname, text='Generate')
+
+    def display_bake_section(self, context):
+        self.layout.operator(bake_operators.BakeSteeringOperator.bl_idname)
+        self.layout.operator(bake_operators.BakeWheelRotationOperator.bl_idname)
+        self.layout.operator(bake_operators.ClearSteeringWheelsRotationOperator.bl_idname)
+
+    def display_rig_props_section(self, context):
+        layout = self.layout.column()
+        layout.prop(context.object, '["wheels_on_y_axis"]', text="Wheels on Y axis")
+        layout.prop(context.object, '["suspension_factor"]', text="Suspension fact.")
+        layout.prop(context.object, '["suspension_rolling_factor"]', text="Suspension rolling fact.")
+
+    def display_ground_sensors_section(self, context, bones):
+        ground_sensors_name = [b.name for b in bones if b.name.startswith('GroundSensor.')]
+        for name in ground_sensors_name:
+            ground_projection_constraint = context.object.pose.bones[name].constraints.get('Ground projection')
+            if ground_projection_constraint is not None:
+                self.layout.label(text="%s:" % name, icon='BONE_DATA')
+                self.layout.prop(ground_projection_constraint, 'target', text='Ground')
+                if ground_projection_constraint.target is not None:
+                    self.layout.prop(ground_projection_constraint, 'shrinkwrap_type')
+                    if ground_projection_constraint.shrinkwrap_type == 'PROJECT':
+                        self.layout.prop(ground_projection_constraint, 'project_limit')
+                    self.layout.prop(ground_projection_constraint, 'influence')
+            ground_projection_limit_constraint = context.object.pose.bones[name].constraints.get('Ground projection limitation')
+            if ground_projection_limit_constraint is not None:
+                self.layout.prop(ground_projection_limit_constraint, 'min_z', text='min Z')
+                self.layout.prop(ground_projection_limit_constraint, 'max_z', text='max Z')
             self.layout.separator()
-            self.layout.prop(context.object, '["wheels_on_y_axis"]', text="Wheels on Y axis")
-            self.layout.prop(context.object, '["suspension_factor"]', text="Suspension fact.")
-            self.layout.prop(context.object, '["suspension_rolling_factor"]', text="Suspension rolling fact.")
-
-            if bpy.context.selected_pose_bones is not None:
-                ground_sensors_name = [b.name for b in bpy.context.selected_pose_bones if b.name.startswith('GroundSensor.')]
-                for name in ground_sensors_name:
-                    ground_projection_constraint = context.object.pose.bones[name].constraints.get('Ground projection')
-                    self.layout.separator()
-                    if ground_projection_constraint is not None:
-                        self.layout.label(text="%s:" % name)
-                        self.layout.prop(ground_projection_constraint, 'target', text='Ground')
-                        if ground_projection_constraint.target is not None:
-                            self.layout.prop(ground_projection_constraint, 'shrinkwrap_type')
-                            if ground_projection_constraint.shrinkwrap_type == 'PROJECT':
-                                self.layout.prop(ground_projection_constraint, 'project_limit')
-                            self.layout.prop(ground_projection_constraint, 'influence')
-                    ground_projection_limit_constraint = context.object.pose.bones[name].constraints.get('Ground projection limitation')
-                    if ground_projection_limit_constraint is not None:
-                        row = self.layout.row()
-                        row.prop(ground_projection_limit_constraint, 'min_z', text='min Z')
-                        row.prop(ground_projection_limit_constraint, 'max_z', text='max Z')
-
-        elif context.object.mode in {"POSE", "OBJECT"}:
-            self.layout.operator(car_rig.GenerateCarAnimationRigOperator.bl_idname, text='Generate')
 
 
-class UICarRigPropertiesPanel(bpy.types.Panel, BaseCarRigPanel):
-    bl_label = "Car Rig"
+class UIRigacarRigPropertiesPanel(bpy.types.Panel, BaseCarRigPanel):
+    bl_label = "Rigacar"
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
     bl_context = "data"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    @classmethod
+    def poll(cls, context):
+        return BaseCarRigPanel.is_car_rig(context)
+
+    def draw(self, context):
+        if BaseCarRigPanel.is_car_rig_generated(context):
+            self.display_bake_section(context)
+            self.layout.separator()
+            self.display_rig_props_section(context)
+        else:
+            self.display_generate_section(context)
 
 
-class UICarRigView3DPanel(bpy.types.Panel, BaseCarRigPanel):
-    bl_label = "Car Rig"
+class UIRigacarGroundSensorsPropertiesPanel(bpy.types.Panel, BaseCarRigPanel):
+    bl_label = "Ground Sensors"
+    bl_parent_id = "UIRigacarRigPropertiesPanel"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "data"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    @classmethod
+    def poll(cls, context):
+        return BaseCarRigPanel.is_car_rig_generated(context)
+
+    def draw(self, context):
+        self.display_ground_sensors_section(context, bpy.context.object.data.bones)
+
+
+class UIRigacarAnimationRigView3DPanel(bpy.types.Panel, BaseCarRigPanel):
+    bl_category = "Rigacar"
+    bl_label = "Animation Rig"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
+
+    @classmethod
+    def poll(cls, context):
+        return BaseCarRigPanel.is_car_rig(context)
+
+    def draw(self, context):
+        if BaseCarRigPanel.is_car_rig_generated(context):
+            self.display_rig_props_section(context)
+        else:
+            self.display_generate_section(context)
+
+
+class UIRigacarWheelsAnimationView3DPanel(bpy.types.Panel, BaseCarRigPanel):
+    bl_category = "Rigacar"
+    bl_label = "Wheels animation"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+
+    @classmethod
+    def poll(cls, context):
+        return BaseCarRigPanel.is_car_rig_generated(context)
+
+    def draw(self, context):
+        self.display_bake_section(context)
+
+
+class UIRigacarGroundSensorsView3DPanel(bpy.types.Panel, BaseCarRigPanel):
+    bl_category = "Rigacar"
+    bl_label = "Ground Sensors"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    @classmethod
+    def poll(cls, context):
+        return BaseCarRigPanel.is_car_rig_generated(context)
+
+    def draw(self, context):
+        self.display_ground_sensors_section(context, bpy.context.object.data.bones)
 
 
 def menu_entries(menu, context):
     menu.layout.operator(car_rig.AddCarDeformationRigOperator.bl_idname, text="Car (deformation rig)", icon='AUTO')
 
 
+classes = (
+  UIRigacarRigPropertiesPanel,
+  UIRigacarGroundSensorsPropertiesPanel,
+  UIRigacarAnimationRigView3DPanel,
+  UIRigacarWheelsAnimationView3DPanel,
+  UIRigacarGroundSensorsView3DPanel,
+)
+
+
 def register():
     bpy.types.VIEW3D_MT_armature_add.append(menu_entries)
-    bpy.utils.register_class(UICarRigPropertiesPanel)
-    bpy.utils.register_class(UICarRigView3DPanel)
+    for c in classes:
+        bpy.utils.register_class(c)
     car_rig.register()
     bake_operators.register()
 
 
 def unregister():
-    bpy.types.INFO_MT_armature_add.remove(menu_entries)
-    bpy.utils.unregister_class(UICarRigPropertiesPanel)
-    bpy.utils.unregister_class(UICarRigView3DPanel)
-    car_rig.unregister()
     bake_operators.unregister()
+    car_rig.unregister()
+    for c in classes:
+        bpy.utils.unregister_class(c)
+    bpy.types.VIEW3D_MT_armature_add.remove(menu_entries)
 
 
 if __name__ == "__main__":
